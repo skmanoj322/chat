1409a6dcd985abb1a10fd5de47c0b54ddf27fe56
Manoj sk <86718126+skmanoj322@users.noreply.github.com>
2024-09-22 20:42:10 +0530

    askdasd

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ const redirectUri = "http://localhost:3000/auth/callback/google";
 
 app.use("/chat", authenticateToken, routerV1);
 app.get("/", (req, res) => {
-  res.send("Hello aws 123");
+  res.send("Hello aws 1234");
 });
 wss.WebSocketConnect(socketHandler);
 server.listen(port, () => {
