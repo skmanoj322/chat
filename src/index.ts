d7ec8e87fc47fd11a27f369eb74b36fe16994d21
Manoj sk <86718126+skmanoj322@users.noreply.github.com>
2024-09-23 00:36:02 +0530

    next chapter

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ const redirectUri = "http://localhost:3000/auth/callback/google";
 
 app.use("/chat", authenticateToken, routerV1);
 app.get("/", (req, res) => {
-  res.send("Lol");
+  res.send("CHATGPt");
 });
 wss.WebSocketConnect(socketHandler);
 server.listen(port, () => {
